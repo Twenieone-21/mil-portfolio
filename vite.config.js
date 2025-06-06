8e483fec336abfaa22bdffe6d6fe6feed0fb4dcb
Twenieone-21 <balmescamelle21@gmail.com>
2025-06-06 17:56:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Use "/" if deploying to root on Vercel
-  base: "/",
-  // PostCSS config usually auto-detected; can omit this
-  // css: {
-  //   postcss: './postcss.config.js'
-  // }
 })
